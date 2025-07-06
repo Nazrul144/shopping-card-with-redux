374dba31d9ac0107cfa2395c678be43d249bf182
Nazrul Islam <nazrulislam.cse28@gmail.com>
2025-07-07 02:59:44 +0600

    Card added

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux"
+import ProductCard from "./ProductCard"
 
 const Home = () => {
   const products = useSelector((state: string)=> state.cart)
@@ -7,7 +8,11 @@ const Home = () => {
     <div>
         <h1 className="text-red-400">Welcome to the Shopping Cart</h1>
         <div className='grid grid-cols-3 gap-4'>
-          <h1>Product Cart</h1>
+          {
+            products.map((product: any) => (
+              <ProductCard key={product.id} product={product} />
+            ))
+          }
             <div className='grid grid-cols-1'>
               <h1>Add to Cart</h1>
             </div>
