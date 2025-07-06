ff4a916ca9226d6ddd1c7025bc310cb14153eeeb
Nazrul Islam <nazrulislam.cse28@gmail.com>
2025-07-07 01:08:00 +0600

    Navbar added

--- src/layout/Root.tsx
+++ src/layout/Root.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/shared/Footer"
 import Navbar from "@/components/shared/Navbar"
 import { Outlet } from "react-router"
 
@@ -5,8 +6,9 @@ import { Outlet } from "react-router"
 const Root = () => {
   return (
     <div>
-        <Navbar/>
-       <Outlet/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
   )
 }
