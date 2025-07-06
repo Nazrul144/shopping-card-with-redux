bedf3379fe933fe188f3a07415e7025c2790c381
Nazrul Islam <nazrulislam.cse28@gmail.com>
2025-07-07 00:55:42 +0600

    routing set up

---
+++ src/layout/Root.tsx
@@ -0,0 +1,16 @@
+
+
+const Root = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+        <p className="text-lg text-gray-700">
+            Welcome to your shopping cart application.
+        </p>
+    </div>
+  )
+}
+
+export default Root
